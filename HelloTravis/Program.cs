fed6e89dc08c253868f1751374c11ac760f64b2a
László Blázovics <blazovics.laszlo@aut.bme.hu>
2020-02-25 11:40:00 +0100

    First Commit

---
+++ HelloTravis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloTravis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Travis!");
+        }
+    }
+}
